c5f739276195f0ecfbf4e1979036a00df9f7332e
Sijohn Devoteam <sijohn.mathew@devoteam.com>
2022-02-01 13:23:30 +0100

    Updated Ttile

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          All for React-2 ❤️.
+          Indussss ❤️ Sijohn
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          -- Sijohn Mathew
+          -- Sijohn Mathew 13.22
         </a>
       </header>
     </div>
