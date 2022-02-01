51399866b2d1e066f1c0f97b72eada7c2a5c73a6
Sijohn Devoteam <sijohn.mathew@devoteam.com>
2022-02-01 13:39:02 +0100

    Updated Sijohn L React

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Indussss ❤️ Sijohn
+		Sijohn ❤️ React
         </p>
         <a
           className="App-link"
